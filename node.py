9b2bb9e37daf75a20ad5b8bf6d1035a2e0fa725f
Nanx3 <nancyespinosax3@gmail.com>
2017-02-24 11:09:32 -0600

    tab

--- node.py
+++ node.py
@@ -1,6 +1,6 @@
 class TreeNode():
     def __init__(self, parentNode, state, movement, cost ):
-    	self.parentNode = parentNode
+        self.parentNode = parentNode
         self.state = state      
         self.movement = movement
         self.cost = cost
