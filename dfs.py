6851b29b5756eaf742207f937ebf7069c6089f51
Nanx3 <nancyespinosax3@gmail.com>
2017-02-24 10:57:57 -0600

    dfs

---
+++ dfs.py
@@ -0,0 +1,80 @@
+from node import TreeNode
+from inputReader import  InputReader
+import copy
+
+def searchingRecursive(initialState, visitedNodes):
+    node = initialState
+    nodeInitialState = node.state
+    stateLength = len(nodeInitialState)
+    cost = node.cost
+    if(nodeInitialState == goalState):
+        return node
+    else:
+        for i in range(0, stateLength):
+            for j in range(0, stateLength):
+                if (i != j and (len(nodeInitialState[j])) > 0 and (len(nodeInitialState[i])) < maxHeight):
+                    nstate = copy.deepcopy(nodeInitialState)
+                   
+                    #Put the last container j -> i 
+                    nstate[i].append(nstate[j].pop())
+                    if (not(visitedNodes.count(nstate))):
+                        movements = [i, j]
+                        
+                        #New cost
+                        #1 Picking up the container and Putting the container down + 1 for Moving the container one stack to the left or right + the accumulated cost of the path
+                        newCost = 1 + abs(i - j) + cost
+                        
+                        #New node
+                        newNode = TreeNode(node, nstate, movements, newCost)
+                        
+                        #Visited list
+                        visitedNodes.append(nstate)
+
+                        #Recursive call
+                        auxDFS = searchingRecursive(newNode, visitedNodes)
+                        if(auxDFS != None):
+                            return auxDFS
+    return None
+
+def searchingByDFS():
+    visitedNodes = []
+    auxDFS = searchingRecursive(initialState, visitedNodes)
+    if(auxDFS == None):
+        return None
+    else:
+        return auxDFS
+
+#Print the path
+def path(node):
+    actual = node
+    steps = []
+    if(node != None):
+        print(node.cost)
+    while(actual != None):
+        if(actual.parentNode != None):
+            steps.append(actual.movement)
+        actual = actual.parentNode
+    while(len(steps) > 0):
+        print(steps.pop()),
+
+#Read the input file
+reader = InputReader()
+data = reader.readDFS()
+#print data
+
+#Store the reading results in the global variables
+
+#Creating the node for the initial configuration
+initialConfiguration = TreeNode(None, data[1], None, 0)
+
+#Pointing to the global variables
+goalState = data[2]
+maxHeight = data[0]
+initialState = initialConfiguration
+
+#Search
+auxDFS = searchingByDFS()
+if (auxDFS != None):
+    path(auxDFS) #Print solution
+else:
+    print ("No solution found")
--- inputReader.py
+++ inputReader.py
@@ -21,3 +21,34 @@ class InputReader():
                         goalState[i].pop()
             numberOfLine += 1
         return maxHeight, initialConfiguration, goalState
+
+    def readDFS(self):
+        input = fileinput.input() #Input
+        i = 0
+        for line in input:  #Read line by line
+            i += 1
+            if i == 1:  #Reads first line (maximum stack height)
+                maxHeight = int(line.strip())
+            elif i == 2:  #Reads second line (intial state)
+                initialConfiguration = line.strip()
+                initialConfiguration = initialConfiguration.replace(" ", "")
+                initialConfiguration = initialConfiguration.replace("(", "")
+                initialConfiguration = initialConfiguration.replace(")", "")
+                initialConfiguration = initialConfiguration.split(';')
+                for i, element in enumerate(initialConfiguration):
+                    initialConfiguration[i] = element.split(",")
+                    if (initialConfiguration[i] == ['']):
+                        initialConfiguration[i].pop()
+            else:  #Reads the third line (end state)
+                goalState = line.strip()
+                goalState = goalState.replace(" ", "")
+                goalState = goalState.replace("(", "")
+                goalState = goalState.replace(")", "")
+                goalState = goalState.replace("X", "")
+                goalState = goalState.split(';')
+                for i, element in enumerate(goalState):
+                    goalState[i] = element.split(",")
+                    if (goalState[i] == ['']):
+                        goalState[i].pop()
+                break
+        return  maxHeight, initialConfiguration, goalState
